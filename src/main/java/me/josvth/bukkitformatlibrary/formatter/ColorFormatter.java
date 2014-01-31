6abc63b3a47787b3eb23eb81e108919f249d8500
Jos <josvth@live.nl>
2014-01-31 23:42:02 +0100

    Fixed NPE in ColorFormatter.

--- src/main/java/me/josvth/bukkitformatlibrary/formatter/ColorFormatter.java
+++ src/main/java/me/josvth/bukkitformatlibrary/formatter/ColorFormatter.java
@@ -6,45 +6,49 @@ import java.util.Map;
 
 public class ColorFormatter extends Formatter {
 
-	private static final char DEFAULT_COLOR_CHAR = '&';
-	private static final ChatColor DEFAULT_MAIN_COLOR = null;
+    private static final char DEFAULT_COLOR_CHAR = '&';
+    private char colorChar = DEFAULT_COLOR_CHAR;
+    private static final ChatColor DEFAULT_MAIN_COLOR = null;
+    private ChatColor mainColor = null;
 
-	private char colorChar = DEFAULT_COLOR_CHAR;
-	private ChatColor mainColor = null;
+    public ColorFormatter(String name) {
+        super(name);
+    }
 
-	public ColorFormatter(String name) {
-		super(name);
-	}
+    public ColorFormatter(String name, char colorChar, ChatColor mainColor) {
+        super(name);
+        this.colorChar = colorChar;
+        this.mainColor = mainColor;
+    }
 
-	public ColorFormatter(String name, char colorChar, ChatColor mainColor) {
-		super(name);
-		this.colorChar = colorChar;
-		this.mainColor = mainColor;
-	}
+    public static final ColorFormatter deserialize(String name, Map<String, Object> settings) {
 
-	@Override
-	public String format(String message) {
-        final StringBuilder builder = new StringBuilder();
-		if (mainColor != null) {
-			builder.append(mainColor);
-		}
-		builder.append(ChatColor.translateAlternateColorCodes(colorChar, message));
-		return builder.toString();
-	}
+        char colorChar = DEFAULT_COLOR_CHAR;
+        if (settings.get("color-char") instanceof String) {
+            colorChar = ((String) settings.get("color-char")).charAt(0);
+        }
 
-	public static final ColorFormatter deserialize(String name, Map<String, Object> settings) {
+        ChatColor mainColor = DEFAULT_MAIN_COLOR;
+        if (settings.get("color-color") instanceof String) {
+            mainColor = ChatColor.getByChar((String) settings.get("color-color"));
+        }
 
-		char colorChar = DEFAULT_COLOR_CHAR;
-		if (settings.get("color-char") instanceof String) {
-			colorChar = ((String) settings.get("color-char")).charAt(0);
-		}
+        return new ColorFormatter(name, colorChar, mainColor);
 
-		ChatColor mainColor = DEFAULT_MAIN_COLOR;
-		if (settings.get("color-color") instanceof String) {
-			mainColor = ChatColor.getByChar((String) settings.get("color-color"));
-		}
+    }
 
-		return new ColorFormatter(name, colorChar, mainColor);
+    @Override
+    public String format(String message) {
+        if (message != null) {
 
-	}
+            final StringBuilder builder = new StringBuilder();
+            if (mainColor != null) {
+                builder.append(mainColor);
+            }
+            builder.append(ChatColor.translateAlternateColorCodes(colorChar, message));
+            return builder.toString();
+
+        }
+        return message;
+    }
 }
